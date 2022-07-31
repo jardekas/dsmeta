7dff809fac872bc23eeac26a466a104eaacc0cd2
jardekas <jardel.beylian@gmail.com>
2022-07-31 14:26:03 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,21 @@
 import icon from '../../assets/img/notification-icon.svg';
+import axios from 'axios';
+import './styles.css';
+import { BASE_URL } from '../../utils/request';
 
-import './styles.css'
+type Props = {
+    saleId:number;
+}
 
-function NotificationButton() {
+function handleClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(Response => {
+            console.log("ENVIADO");
+        })
+}
+function NotificationButton( {saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar"/>
         </div>
     )
